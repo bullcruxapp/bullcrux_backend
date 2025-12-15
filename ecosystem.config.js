cda128ae747cf2df38c3a6b06750cf0a66028640
Ezequiel <ezepuglisi@gmail.com>
2025-12-15 20:02:33 -0300

    configurando entorno dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'backend',
+      name: 'bullcrux_backend_prod',
       script: './dist/src/main.js',
       watch: false,
       env: {
         NODE_ENV: 'production'
+      }
+    },
+    {
+      name: 'bullcrux_backend_dev',
+      script: './dist/src/main.js',
+      watch: true,
+      env: {
+        NODE_ENV: 'development'
       }
     }
   ]
