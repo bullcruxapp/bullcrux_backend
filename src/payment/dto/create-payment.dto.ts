e93aaaf3be1f1afc491cf64c559d0384adc9c183
bullcruxapp <bullcruxapp@gmail.com>
2026-06-12 23:14:08 -0300

    Create create-payment.dto.ts

---
+++ src/payment/dto/create-payment.dto.ts
@@ -0,0 +1,12 @@
+import { IsInt, IsString, IsNotEmpty, Min, Max } from 'class-validator';
+
+export class CreatePaymentDto {
+  @IsString()
+  @IsNotEmpty()
+  raffleId: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  quantity: number;
+}
